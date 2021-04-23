59350ddaf9abcdf95941e1f4e210c0e2f4a0b94f
Lena Schimmel <mail@lenaschimmel.de>
2021-04-23 21:04:45 +0200

    Simplify ressource loading with iterator.

--- main.lua
+++ main.lua
@@ -14,6 +14,20 @@ CANVAS_HEIGHT = 1080
 roomy = libroomy.new() -- roomy is the manager for all the rooms (scenes) in our game
 scenes = {} -- initialize list of scenes
 
+function listUsefulFiles(dirname)
+    f, t, k = pairs(love.filesystem.getDirectoryItems(dirname))
+    return function ()
+        k,v = f(t, k)
+        if v and v == ".gitkeep" then
+            k,v = f(t, k)
+        end
+        if v then
+            basename = v:match("(.+)%..+$") -- remove everything before the last period
+            return {basename = basename, filename = v, path = (dirname .. "/" .. v)}
+        end
+    end
+end
+
 function love.load()
     -- initialize randomness in two ways:
     love.math.setRandomSeed(os.time())
@@ -24,34 +38,26 @@ function love.load()
 
     -- load assets
     images = {}
-    for i,filename in pairs(love.filesystem.getDirectoryItems("images")) do
-        if filename ~= ".gitkeep" then
-            images[filename:sub(1,-5)] = love.graphics.newImage("images/"..filename)
-        end
+    for file in listUsefulFiles("images") do
+        images[file.basename] = love.graphics.newImage(file.path)
     end
 
     sounds = {}
-    for i,filename in pairs(love.filesystem.getDirectoryItems("sounds")) do
-        if filename ~= ".gitkeep" then
-            sounds[filename:sub(1,-5)] = love.audio.newSource("sounds/"..filename, "static")
-        end
+    for file in listUsefulFiles("sounds") do
+        sounds[file.basename] = love.audio.newSource(file.path, "static")
     end
 
     music = {}
-    for i,filename in pairs(love.filesystem.getDirectoryItems("music")) do
-        if filename ~= ".gitkeep" then
-            music[filename:sub(1,-5)] = love.audio.newSource("music/"..filename, "stream")
-            music[filename:sub(1,-5)]:setLooping(true)
-        end
+    for file in listUsefulFiles("music") do
+        music[file.basename] = love.audio.newSource(file.path, "stream")
+        music[file.basename]:setLooping(true)
     end
 
     fonts = {}
-    for i,filename in pairs(love.filesystem.getDirectoryItems("fonts")) do
-        if filename ~= ".gitkeep" then
-            fonts[filename:sub(1,-5)] = {}
-            for fontsize=50,100 do
-                fonts[filename:sub(1,-5)][fontsize] = love.graphics.newFont("fonts/"..filename, fontsize)
-            end
+    for file in listUsefulFiles("fonts") do
+        fonts[file.basename] = {}
+        for fontsize=50,100 do
+            fonts[file.basename][fontsize] = love.graphics.newFont(file.path, fontsize)
         end
     end
 
@@ -59,23 +65,17 @@ function love.load()
 
     -- levels
     ind = 0
-    for i,filename in pairs(love.filesystem.getDirectoryItems("levels")) do
+    for file in listUsefulFiles("levels") do
         -- TODO: the list of filenames should be sorted to ensure the correct order of levels
-        if filename ~= ".gitkeep" then
-            ind = ind + 1
-            local levelName = filename:sub(1, -5)
-            levels[ind] = require ("levels."..levelName)
-        end
+        ind = ind + 1
+        levels[ind] = require ("levels." .. file.basename)
     end
 
     initLevelManager() -- this must be done after the levels are loaded from the filesystem
 
     -- scenes
-    for i,filename in pairs(love.filesystem.getDirectoryItems("scenes")) do
-        if filename ~= ".gitkeep" then
-            local sceneName = filename:sub(1, -5)
-            scenes[sceneName] = require ("scenes."..sceneName)
-        end
+    for file in listUsefulFiles("scenes") do
+        scenes[file.basename] = require ("scenes." .. file.basename)
     end
     roomy:hook({exclude = {"draw", "update"}}) --hook roomy in to manage the scenes (with exceptions)
     roomy:enter(scenes.title) --start on title screen
