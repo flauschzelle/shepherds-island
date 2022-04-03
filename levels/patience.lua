a9e6623b60d1c4130d13ec600e128992a4f3d5ce
blinry <mail@blinry.org>
2022-04-03 17:09:33 +0200

    Two new water levels

---
+++ levels/patience.lua
@@ -0,0 +1,17 @@
+name = "Basin of patience"
+map = [[
+
+
+     g
+    gg
+   ggg               a
+h gggg               g
+wgggggbbbbbbb p      g
+gggggggggggggggggggggg
+]]
+
+intro = ""
+
+level = Level:new(name, map)
+
+return level
---
+++ levels/rescue_mission.lua
@@ -0,0 +1,16 @@
+name = "Rescue mission"
+map = [[
+
+
+         a a bgggggggh
+    a p gggggggggggggw
+wwwggggggggggggggggggw
+wwgggggggggggggggggggw
+wggggggggggggggggggggw
+]]
+
+intro = ""
+
+level = Level:new(name, map)
+
+return level
