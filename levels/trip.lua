dab18e1e272dddf0855ae46ed9192ed8ee787c7f
blinry <mail@blinry.org>
2022-04-03 17:33:55 +0200

    Dry dock level

---
+++ levels/trip.lua
@@ -0,0 +1,16 @@
+name = "Dry dock"
+map = [[
+
+
+   g
+  gg                 a  a
+wggg p h           ggggggggg
+gggggggggwwwwwwwgggggggggggg
+gggggggggggggggggggggggggggg
+]]
+
+intro = ""
+
+level = Level:new(name, map)
+
+return level
