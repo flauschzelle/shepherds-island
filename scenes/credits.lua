19249c7da232b6ca27c58f6f2d1862a8af6f7097
blinry <mail@blinry.org>
2022-04-04 21:29:07 +0200

    Nice message on credits screen

--- scenes/credits.lua
+++ scenes/credits.lua
@@ -2,12 +2,15 @@ local scene = {}
 
 function scene:draw()
     love.graphics.setColor(1, 1, 1) --white
-    love.graphics.printf("Thanks for playing!", 0, CANVAS_HEIGHT/2, CANVAS_WIDTH, "center")
+    love.graphics.printf("Thanks for playing! <3", 0, CANVAS_HEIGHT*1/4, CANVAS_WIDTH, "center")
+
+    love.graphics.printf("If you enjoyed this game, say hi on Twitter! :)\n\n@flauschzelle   @blinry", CANVAS_WIDTH/4, CANVAS_HEIGHT*2/4, CANVAS_WIDTH/2, "center")
 end
 
 function scene:handleInput()
     if input:isPressed("quit") then
-        love.event.quit()
+        roomy:enter(scenes.gameplay)
+        roomy:push(scenes.menu)
     end
 end
 
